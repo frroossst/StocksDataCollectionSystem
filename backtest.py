eba54fdb559a06be6abba0ef7ce2ffc9e69b1831
frroossst <adhyanpatel@gmail.com>
2021-07-22 15:12:41 +0530

    0.0.1 - initial setup

---
+++ backtest.py
@@ -0,0 +1,33 @@
+import yfinance as yf
+import pandas as pd
+from matplotlib import pyplot as plt
+import json
+
+# Basic Stocks Trading Strategy
+
+# [BUY]
+# 1. 26 week high is greater than 52 week high
+# 2. RSI > 70 
+# 3. ADX > 25
+# 4. MACD signal is true
+# 5. 50 day EMA is greater than 200 day EMA
+# 6. OBV signal is up!
+
+# [SELL]
+# 1. 26 week average is less than 52 week high
+# 2. RSI > 75
+# 3. ADX > 25
+# 4. MACD is set to sell
+# 5. current price is 0.70 * bought price
+
+nvda = yf.Ticker("NVDA")
+
+historical = nvda.info
+
+currentPrice = nvda.info["regularMarketPrice"]
+fiftyTwoWeekHigh = nvda.info["fiftyTwoWeekHigh"]
+
+if currentPrice > fiftyTwoWeekHigh:
+    print("Current price is greater than 52 week high")
+else:
+    print("Current price is less than 52 week high")
