3a17df9e4218c3ee93fb19fa496e86cfa6932b44
frroossst <adhyanpatel@gmail.com>
2021-07-22 19:50:37 +0530

    0.0.3 - search query and lookup

--- backtest.py
+++ backtest.py
@@ -2,43 +2,64 @@ import yfinance as yf
 import pandas as pd
 from matplotlib import pyplot as plt
 import json
-import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# Basic Stocks Trading Strategy
+# # # Basic Stocks Trading Strategy
 
-# [BUY]
-# 1. 26 week high is greater than 52 week high
-# 2. RSI > 70 
-# 3. ADX > 25
-# 4. MACD signal is true
-# 5. 50 day EMA is greater than 200 day EMA
-# 6. OBV signal is up!
+# # # [BUY]
+# # # 1. 26 week high is greater than 52 week high
+# # # 2. RSI > 70 
+# # # 3. ADX > 25
+# # # 4. MACD signal is true
+# # # 5. 50 day EMA is greater than 200 day EMA
+# # # 6. OBV signal is up!
 
-# [SELL]
-# 1. 26 week average is less than 52 week high
-# 2. RSI > 75
-# 3. ADX > 25
-# 4. MACD is set to sell
-# 5. current price is 0.70 * bought price
+# # # [SELL]
+# # # 1. 26 week average is less than 52 week high
+# # # 2. RSI > 75
+# # # 3. ADX > 25
+# # # 4. MACD is set to sell
+# # # 5. current price is 0.70 * bought price
 
-is52weekHigh = False
-isRSI = False
-isADX = False
-isMACD = False
-is50dayEMA = False
-isOBV = False                                                                
+PATH = "/home/home/Desktop/Projects/Stonks/chromedriver"
+driver = webdriver.Chrome(PATH)
 
-nvda = yf.Ticker("NVDA")
+driver.get("https://in.finance.yahoo.com/")
 
-historical = nvda.info
+search = driver.find_element_by_id("yfin-usr-qry")
+search.send_keys("NVDA")
+search.send_keys(Keys.RETURN)
 
-currentPrice = nvda.info["regularMarketPrice"]
-fiftyTwoWeekHigh = nvda.info["fiftyTwoWeekHigh"]
 
-if currentPrice > fiftyTwoWeekHigh:
-    print("Current price is greater than 52 week high")
-    is52weekHigh = True
-else:
-    print("Current price is less than 52 week high")
-    is52weekHigh = False
+time.sleep(60)
+
+driver.quit()
+
+
+
+
+
+
+# is52weekHigh = False
+# isRSI = False
+# isADX = False
+# isMACD = False
+# is50dayEMA = False
+# isOBV = False                                                                
+
+
+# nvda = yf.Ticker("NVDA")
+# historical = nvda.info
+# currentPrice = nvda.info["regularMarketPrice"]
+# fiftyTwoWeekHigh = nvda.info["fiftyTwoWeekHigh"]
+
+# if currentPrice > fiftyTwoWeekHigh:
+#     print("Current price is greater than 52 week high")
+#     is52weekHigh = True
+# elif currentPrice < fiftyTwoWeekHigh:
+#     print("Current price is less than 52 week high")
+#     is52weekHigh = False
+
 
