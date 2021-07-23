bbbab3e922989ec6f046d658f9c0420273cd6636
frroossst <adhyanpatel@gmail.com>
2021-07-23 19:56:07 +0530

    0.0.8 - bs4 object is now JSON serialized

--- backtest.py
+++ backtest.py
@@ -6,7 +6,6 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-
 # # # Basic Stocks Trading Strategy
 
 # # # [BUY]
@@ -24,8 +23,6 @@ from bs4 import BeautifulSoup
 # # # 4. MACD is set to sell
 # # # 5. current price is 0.70 * bought price
 
-# BS4 method
-
 companies = ["AAPL","MSFT","AMZN","GOOGL","FB","NVDA","PYPL","NFLX","CMCSA","INTC","ADBE","AMD","TSM",
 "PEP","CSCO","AVGO","QCOM","TMUS","COST","KO","TXN","AMGN","CHTR","SBUX","ABNB","AMAT","ISRG","MU","GILD"]
 
@@ -33,15 +30,24 @@ symbol = companies[5]
 
 headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"}
 
-# url = "https://finance.yahoo.com/quote/{symbol}/key-statistics?p={symbol}"
-# r = requests.get(url, headers= headers)
-# soup = BeautifulSoup(r.text,"html.parser")
-
-# print(soup.prettify())
-
 url = ("https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=Pkgha4QtRuo&lang=en-US&region=US&symbols=NVDA&fields=messageBoardId,longName,shortName,marketCap,underlyingSymbol,underlyingExchangeSymbol,headSymbolAsString,regularMarketPrice,regularMarketChange,regularMarketChangePercent,regularMarketVolume,uuid,regularMarketOpen,fiftyTwoWeekLow,fiftyTwoWeekHigh,toCurrency,fromCurrency,toExchange,fromExchange&corsDomain=finance.yahoo.com")
 r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text,"html.parser")
 
-for i in soup:
-    print(i)
+print(type(soup))
+
+result = soup.string
+
+with open("data.json","w") as fobj:
+    json.dump(result,fobj)
+    fobj.close()
+
+
+
+
+
+
+
+
+
+
