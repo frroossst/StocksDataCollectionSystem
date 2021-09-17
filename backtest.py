a8d54ff97bb5dd035894d1aa81d69fd004450f78
frroossst <adhyanpatel@gmail.com>
2021-09-17 17:54:51 +0530

    parse retrieved data into scrips

--- backtest.py
+++ backtest.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import datetime
 import json
 import math
-from typing_extensions import TypeGuard
 
 hotli = []
 
--- redFlags.py
+++ redFlags.py
@@ -1,17 +1,42 @@
-from bs4 import BeautifulSoup
-import bs4 as bs
-import requests
+from selenium import webdriver
 import json
+import time
 
 headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"}
+
+asmShort = []
+asmLong = []
+gsm = []
+
+with open("settings.json","r") as fobj:
+    content = json.load(fobj)
+    fobj.close()
+
+PATH = content["path"]
 
 urlASM = "https://www.nseindia.com/reports/asm"
 urlGSM = "https://www.nseindia.com/reports/gsm"
 
-sourceASM = requests.get(urlASM, headers=headers).text
-sourceGSM = requests.get(urlGSM, headers=headers).text
+driver = webdriver.Chrome(PATH)
+driver.get(urlASM)
+time.sleep(1.5)
+search = driver.find_element_by_id("asmLTTable")
+search = search.text
+li = search.split(" ")
+leng = len(li)
 
-soupASM = bs.BeautifulSoup(sourceASM,"lxml")
-soupGMS = bs.BeautifulSoup(sourceGSM,"lxml")
+count = 0
+while count < leng:
+    print(li[count])
+    try:
+        print(type(li[count]))
+        print(int(li[count]))
+        if int(li[count]).isnum():
+            asmLong.append(li[count +1])
+        else:
+            count += 1
+    except:
+        count += 1
+        continue
 
-print(soupASM)
+print(asmLong)
