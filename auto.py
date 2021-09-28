b2974757bb2edc3464dd7189fdc7364a8ce29b7d
frroossst <adhyanpatel@gmail.com>
2021-09-28 12:12:58 +0530

    0.7.9 - redFlag checks are performed only once a day

--- auto.py
+++ auto.py
@@ -1,16 +1,52 @@
+from datetime import datetime
+import json
 import time
 
 start = time.time()
 
+def writeFlagLogs():
+    with open("logs.json","r") as fobj:
+        content = json.load(fobj)
+        fobj.close()
+
+    todayDate = datetime.today()
+    todayDate = todayDate.strftime("%d-%m-%Y")
+    content[todayDate] = True
+
+    with open("logs.json","w") as fobj:
+        json.dump(content,fobj,indent=6)
+        fobj.close()
+
+def getFlagLogs() -> bool:
+
+    todayDate = datetime.today()
+    todayDate = todayDate.strftime("%d-%m-%Y")
+
+    with open("logs.json","r") as fobj:
+        content = json.load(fobj)
+        fobj.close()
+
+    try:
+        isRedFlags = content[todayDate]
+    except KeyError:
+        isRedFlags = False
+
+    return isRedFlags
+
 def program():
-    print("[OK] gathering flags")
-    import flags
+    todayRedFlags = getFlagLogs()
+    if not todayRedFlags:
+        print("[OK] gathering flags")
+        import flags
     print("[OK] collecting scrip data")
     import scraper
     print("[OK] running technical analysis")
     import algorithm
     print("[OK] running trading algorithm")
     import backtest
+    writeFlagLogs()
+
+
 
 try:
     program()
