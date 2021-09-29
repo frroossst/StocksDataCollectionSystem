802d7372b93274649ad0be38a22903bf2f10b213
frroossst <adhyanpatel@gmail.com>
2021-09-29 10:37:07 +0530

    send emails and create client lists

--- mail.py
+++ mail.py
@@ -1,2 +1,11 @@
 from datetime import datetime
+import smtplib
+import json
+
+with open("credentials.secret","r") as fobj:
+    cred = json.load(fobj)
+    fobj.close()
+
+emailId = cred["emailId"]
+emailPass = cred["emailPass"]
 
